9ea501c91ff08a28d3d7490fc7dd03f08ee744a8
Rohit Kumar <144173682+roh7itt@users.noreply.github.com>
2026-09-12 13:45:28 +0530

    09-12-2026 0063-unique-paths-ii Medium .java

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -33,3 +33,26 @@ class Solution {
     }
 
 }
+
+/*
+int r = obstacleGrid.length;
+        int c = obstacleGrid[0].length;
+
+        int[][]dp = new int[r][c];
+       // Arrays.fill(dp , 0);
+
+        for(int i = 0 ; i<r ;i++)
+        {
+            for(int j = 0 ; j<c ;j++)
+            {
+                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 && j==0) dp[i][j] = 1;
+                else if(i==0) dp[i][j] = dp[i][j-1];
+                else if(j==0) dp[i][j] = dp[i-1][j];
+                else  dp[i][j]= dp[i][j-1] + dp[i-1][j];
+             
+            }
+        }
+
+        return dp[r-1][c-1];
+ */
