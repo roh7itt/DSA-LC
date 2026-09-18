883d8eddb4a8caa905497caf9ad70a17dc40bd96
Rohit Kumar <144173682+roh7itt@users.noreply.github.com>
2026-09-19 03:19:18 +0530

    09-19-2026 0518-coin-change-ii Medium .java

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -42,3 +42,16 @@ class Solution {
         return dp[n - 1][amount];
     }
 }
+
+/*
+public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for(int coin : coins){
+            for(int i = coin; i <= amount; i++){
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+*/
