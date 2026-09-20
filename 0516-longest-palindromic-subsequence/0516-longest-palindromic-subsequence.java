cd3f4c6c875f52119a6ef102030fa94d128fa02c
Rohit Kumar <144173682+roh7itt@users.noreply.github.com>
2026-09-20 15:10:23 +0530

    09-20-2026 0516-longest-palindromic-subsequence Medium .java

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -4,19 +4,26 @@ class Solution {
         return lcs(s, t);
     }
     private int lcs(String s1, String s2){
-        int n = s1.length() , m = s2.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i =0; i <= n; i++) dp[i][0] = 0;
-        for(int i = 0; i <= m; i++) dp[0][i] = 0;
+        int n = s1.length(), m = s2.length();
+        int[] prev = new int[m + 1]; // Use m+1 since j goes up to m
+        int[] cur = new int[m + 1];
+        
+        // No need for explicit zero-initialization loops (lines 10-11)
+        
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    cur[j] = 1 + prev[j-1];
+                } else {
+                    cur[j] = Math.max(prev[j], cur[j-1]);
                 }
             }
+            // Important: Copy cur to prev for the next iteration
+            for(int k = 0; k <= m; k++){
+                prev[k] = cur[k];
+            }
+            // Optional: Reset cur if needed, but it gets overwritten anyway
         }
-        return dp[n][m];
-    }
+        return cur[m];
+        }
 }
