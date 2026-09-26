8f0960a80b700c0bb5a0f71bb4fa1a12011eb191
Rohit Kumar <144173682+roh7itt@users.noreply.github.com>
2026-09-26 14:23:58 +0530

    09-26-2026 0222-count-complete-tree-nodes Medium .java

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+        int left = getHeightLh(root);
+        int right = getHeightRh(root);
+        if(left == right) return ((2<<(left))-1);
+        else return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    private int getHeightLh(TreeNode root){
+        int count = 0;
+        while(root.left != null){
+            count++;
+            root = root.left;
+        }
+        return count;
+    }
+    private int getHeightRh(TreeNode root){
+        int count = 0;
+        while(root.right != null){
+            count++;
+            root = root.right;
+        }
+        return count;
+    }
+}
